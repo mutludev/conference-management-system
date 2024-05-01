7b49bc713d5ae668739615e7910c166705cde941
Baris Mutlu <baris@mutlu.dev>
2024-05-01 22:40:47 +0300

    Add HomeLayout

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Sidebar from '@/components/Sidebar.vue'
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <Sidebar />
+    <main>
+      <Navbar />
+      <!-- This line may cause problems be AWARE -->
+      <content><RouterView /></content>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+}
+
+content {
+  display: block;
+  flex: 1;
+  overflow: scroll;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import HomeLayout from '../layouts/HomeLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,25 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home-view',
+          component: HomeView
+        }
+      ]
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView
     },
-    { path: '/register', name: 'register', component: RegisterView }
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    }
   ]
 })
 
