1ced563c8bb6fbdf681eb4cc58cf4bd005f81c26
Sultan Delen <99362897+sultandlen@users.noreply.github.com>
2024-05-02 14:28:33 +0300

    Add admin view

---
+++ src/components/MembersHeader.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { SearchOutlined, PlusOutlined } from '@ant-design/icons-vue'
+</script>
+
+<template>
+  <div class="members-header">
+    <h1 class="title">Members</h1>
+    <div class="tools">
+      <div class="member-count">13 members</div>
+      <a-input class="search" type="text" placeholder="Search">
+        <template #prefix>
+          <search-outlined />
+        </template>
+      </a-input>
+      <a-button class="add-button"><PlusOutlined /> Add member</a-button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.members-header {
+  width: 100%;
+  height: 64px;
+  padding: 16px;
+  background-color: hsl(6, 100%, 96%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title {
+  color: hsl(0, 0%, 20%);
+  font-weight: 500;
+}
+
+.tools {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+
+.member-count {
+  color: hsl(0, 0%, 20%);
+  font-size: 14px;
+}
+
+.search {
+  width: 150px;
+}
+
+.add-button {
+  width: 150px;
+  background-color: hsl(11, 100%, 60%);
+  color: white;
+  border: none;
+  border-radius: 5px;
+}
+
+.add-button:hover {
+  color: white;
+  background-color: hsl(11, 100%, 55%);
+  cursor: pointer;
+}
+
+.add-button:active {
+  background-color: hsl(11, 100%, 50%);
+}
+</style>
---
+++ src/views/AdminView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MembersHeader from '@/components/MembersHeader.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <MembersHeader />
+  </div>
+</template>
+
+<style scoped></style>
