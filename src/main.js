2cf983ca0c93dfa7bdffa24a49b0e0c815105c5d
Baris Mutlu <baris@mutlu.dev>
2024-05-08 15:19:41 +0300

    Add dependencies

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
 import router from './router'
 import 'ant-design-vue/dist/reset.css'
 import './style/reset.css'
+import 'vue-toastification/dist/index.css'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
+app.use(Toast)
 app.use(router)
 app.use(createPinia())
 
