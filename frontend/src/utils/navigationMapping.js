3aab67111558311f5af3fcdcbeeee5740196edc5
Baris Mutlu <baris@mutlu.dev>
2024-05-09 23:45:52 +0300

    Add role component mapper

--- frontend/src/utils/navigationMapping.js
+++ frontend/src/utils/navigationMapping.js
@@ -1,12 +1,37 @@
 import { ProjectOutlined, UserOutlined, FileOutlined } from '@ant-design/icons-vue'
+import AdminView from '../views/AdminView.vue'
+import AttendeeView from '../views/AttendeeView.vue'
+import ReviewerView from '../views/ReviewerView.vue'
+import OrganizerView from '@/views/OrganizerView.vue'
+import AuthorEventView from '@/views/AuthorEventView.vue'
+import AuthorView from '@/views/AuthorView.vue'
+
+export const roleComponentMapping = {
+  admin: {
+    home: AdminView
+  },
+  attendee: {
+    home: AttendeeView
+  },
+  reviewer: {
+    home: ReviewerView
+  },
+  organizer: {
+    home: OrganizerView
+  },
+  author: {
+    home: AuthorEventView,
+    papers: AuthorView
+  }
+}
 
 export const linkMapping = {
-  attendee: [{ name: 'Events', path: '/', icon: ProjectOutlined }],
-  admin: [{ name: 'Members', path: '/', icon: UserOutlined }],
-  organizer: [{ name: 'Events', path: '/', icon: ProjectOutlined }],
-  reviewer: [{ name: 'Papers', path: '/', icon: FileOutlined }],
+  attendee: [{ name: 'Events', path: 'home', icon: ProjectOutlined }],
+  admin: [{ name: 'Members', path: 'home', icon: UserOutlined }],
+  organizer: [{ name: 'Events', path: 'home', icon: ProjectOutlined }],
+  reviewer: [{ name: 'Papers', path: 'home', icon: FileOutlined }],
   author: [
-    { name: 'Events', path: '/', icon: ProjectOutlined },
-    { name: 'My Papers', path: '/papers', icon: FileOutlined }
+    { name: 'Events', path: 'home', icon: ProjectOutlined },
+    { name: 'My Papers', path: 'papers', icon: FileOutlined }
   ]
 }
