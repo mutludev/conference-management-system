9fa9abfaa916a53e79c4c78a66a499b323ad8dee
Baris Mutlu <baris@mutlu.dev>
2024-05-11 16:41:59 +0300

    Setup pdf viewer

--- frontend/src/components/PdfModal.vue
+++ frontend/src/components/PdfModal.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import VuePdfApp from 'vue3-pdf-app'
 import 'vue3-pdf-app/dist/icons/main.css'
 import { FileOutlined } from '@ant-design/icons-vue'
+import { VuePDF, usePDF } from '@tato30/vue-pdf'
 
-defineProps(['path'])
+const props = defineProps(['path'])
+
+const { pdf, pages } = usePDF(props.path)
 
 const open = ref(false)
 
@@ -21,8 +23,13 @@ const handleOk = (e) => {
 <template>
   <div>
     <a-button class="view" type="primary" @click="showModal"><FileOutlined />View</a-button>
-    <a-modal :style="{ width: '800px' }" v-model:open="open" title="Paper" @ok="handleOk">
-      <vue-pdf-app style="height: 500px" :pdf="path"></vue-pdf-app>
+    <a-modal :style="{ width: '800px' }" v-model:open="open" @ok="handleOk" :closable="false">
+      <div class="pdf">
+        <div class="pdf-page" v-for="page in pages" :key="page">
+          <VuePDF class="pdf-viewer" :pdf="pdf" :page="page" />
+          <hr />
+        </div>
+      </div>
     </a-modal>
   </div>
 </template>
@@ -31,5 +38,28 @@ const handleOk = (e) => {
 .view {
   border: none;
   box-shadow: none;
+}
+
+.pdf {
+  height: 500px;
+  overflow: scroll;
+}
+
+.pdf-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0;
+}
+
+.pdf-viewer {
+  display: flex !important;
+  justify-content: center;
+}
+
+hr {
+  display: flex;
+  justify-content: center;
+  width: 80%;
 }
 </style>
